67b28f8bbb82bb29fd129f4f385ccccb7304fb7b
Wonwoo Soh <wsoh2@sheffield.ac.uk>
2019-04-12 15:42:40 +0100

    remove unnecessary part

--- mdk/bin/shared/test.py
+++ mdk/bin/shared/test.py
@@ -70,14 +70,6 @@ class controller:
 		# store
 		self.sensors = msg
 
-#	def callback_caml(self, ros_image):
-
-#		self.callback_cam(ros_image, 0)
-
-#	def callback_camr(self, ros_image):
-
-#		self.callback_cam(ros_image, 1)
-
 	def imp_report_wheels(self, msg_wheels):
 
 		if self.report_wheels and not self.sensors is None:
@@ -140,91 +132,6 @@ class controller:
 				print "imu_head", [x.x, x.y, x.z]
 				print "----------------------------------------------------------------"
 
-			# send kin
-			if len(self.kin):
-				msg_kin.position[1] = np.radians(30.0)
-				msg_kin.position[2] = np.radians(0.0)
-				msg_kin.position[3] = np.radians(0.0)
-				if "l" in self.kin:
-					msg_kin.position[1] = xk * np.radians(20.0) + np.radians(30.0)
-				if "y" in self.kin:
-					t = xk * np.radians(45.0)
-					if False:
-						# this branch is used to measure YAW_COUNTS_PER_RAD
-						t = (xk + 0.5) * np.radians(45.0)
-						t = np.clip(t, 0.0, np.radians(45.0))
-					msg_kin.position[2] = t
-				if "p" in self.kin:
-					msg_kin.position[3] = xk * np.radians(15.0) + np.radians(-7.0)
-				self.pub_kin.publish(msg_kin)
-				if not self.kin_sensor is None:
-					a = "[{:.3f}".format(msg_kin.position[1]) + "=" + "{:.3f}]".format(self.kin_sensor[1])
-					b = "[{:.3f}".format(msg_kin.position[2]) + "=" + "{:.3f}]".format(self.kin_sensor[2])
-					c = "[{:.3f}".format(msg_kin.position[3]) + "=" + "{:.3f}]".format(self.kin_sensor[3])
-					#print a, b, c
-
-			# send cos
-			if len(self.cos):
-				sc = 0.5
-				if "h" in self.cos:
-					for i in range(2, 6):
-						msg_cos.data[i] = xc * sc + 0.5
-				if "l" in self.cos:
-					for i in [2, 4]:
-						msg_cos.data[i] = xc * sc + 0.5
-				if "r" in self.cos:
-					for i in [3, 5]:
-						msg_cos.data[i] = xc * sc + 0.5
-				if "y" in self.cos:
-					for i in [2, 3]:
-						msg_cos.data[i] = xc * sc + 0.5
-				if "e" in self.cos:
-					for i in [4, 5]:
-						msg_cos.data[i] = xc * sc + 0.5
-				if "w" in self.cos:
-					msg_cos.data[1] = xc * 0.5 + 0.5
-				if "d" in self.cos:
-					msg_cos.data[0] = xc * 0.5 + 0.5
-				if "x" in self.cos:
-					if xc2 >= 0:
-						msg_cos.data[1] = xc * 0.5 + 0.5
-					else:
-						msg_cos.data[0] = xc * 0.5 + 0.5
-				self.pub_cos.publish(msg_cos)
-
-			# send wheels
-			if not self.wheels is None:
-				v = 0.0
-				Tq = 0.2
-				T = 2.0
-				t1 = Tq
-				t2 = t1 + T
-				t3 = t2 + T
-				t4 = t3 + Tq
-				if t_now < t1:
-					v = 0.0
-				elif t_now < t2:
-					v = (t_now - t1) / T
-				elif self.forever:
-					v = 1.0
-				elif t_now < t3:
-					v = 1.0 - (t_now - t2) / T
-				elif t_now < t4:
-					v = 0.0
-				else:
-					self.active = False
-				msg_wheels.twist.linear.x = v * self.wheels
-				msg_wheels.twist.angular.z = 0.0
-				self.pub_wheels.publish(msg_wheels)
-				self.imp_report_wheels(msg_wheels)
-
-			# send wheels
-			if self.wheelsf:
-				msg_wheels.twist.linear.x = 0.4
-				msg_wheels.twist.angular.z = 0.0
-				self.pub_wheels.publish(msg_wheels)
-				self.imp_report_wheels(msg_wheels)
-
 			# send wheels
 			if self.shoot:
 				msg_kin.position[1] = np.radians(0.0)
@@ -255,7 +162,6 @@ class controller:
 				self.pub_wheels.publish(msg_wheels)
 				self.imp_report_wheels(msg_wheels)
 
-#			if len(self.toss) and not self.sensors is None:
 			if len(self.toss): 
 				if "l" in self.toss:
 					t = xk * np.radians(55.0)
@@ -343,65 +249,6 @@ class controller:
 					self.pub_wheels.publish(msg_wheels)
 					self.imp_report_wheels(msg_wheels)
 
-
-			# send wheels
-			if not self.spin is None:
-				v = 0.0
-				Tq = 0.2
-				T = 1.0
-				t1 = Tq
-				t2 = t1 + T
-				t3 = t2 + T
-				t4 = t3 + Tq
-				if t_now < t1:
-					v = 0.0
-				elif t_now < t2:
-					v = (t_now - t1) / T
-				elif self.forever:
-					v = 1.0
-				elif t_now < t3:
-					v = 1.0 - (t_now - t2) / T
-				elif t_now < t4:
-					v = 0.0
-				else:
-					self.active = False
-				msg_wheels.twist.linear.x = 0.0
-				msg_wheels.twist.angular.z = v * 6.2832 * self.spin
-				self.pub_wheels.publish(msg_wheels)
-				self.imp_report_wheels(msg_wheels)
-
-			# send push
-			if self.push:
-				msg_push.link = miro.constants.LINK_HEAD
-				msg_push.flags = miro.constants.PUSH_FLAG_VELOCITY
-				msg_push.pushpos = geometry_msgs.msg.Vector3(miro.constants.LOC_NOSE_TIP_X, miro.constants.LOC_NOSE_TIP_Y, miro.constants.LOC_NOSE_TIP_Z)
-				msg_push.pushvec = geometry_msgs.msg.Vector3(0.0, 0.2 * xk, 0.0)
-				self.pub_push.publish(msg_push)
-
-			# send illum
-			q = int(xc * 127 + 128)
-			for i in range(0, 3):
-				msg_illum.data[i] = (q << ((2-i) * 8)) | 0xFF000000
-			for i in range(3, 6):
-				msg_illum.data[i] = (q << ((i-3) * 8)) | 0xFF000000
-			self.pub_illum.publish(msg_illum)
-
-			# send P1P
-			if not self.P1P is None:
-				msg_platform = UInt32()
-				msg_platform.data = 0
-				if self.P1P == 'x':
-					self.active = False
-				else:
-					if self.P1P == 'P':
-						msg_platform.data = 1024
-					elif self.P1P == 'O':
-						msg_platform.data = 2048
-					elif self.P1P == 'I':
-						msg_platform.data = 2
-					self.pub_platform.publish(msg_platform)
-					self.P1P = 'x'
-
 			# state
 			time.sleep(0.02)
 			self.count = self.count + 1
@@ -420,23 +267,16 @@ class controller:
 		# input
 		self.report_input = True
 		self.sensors = None
-
 		self.kin_sensor = None
 
 		# options
 		self.report_wheels = False
-		self.wheels = None
-		self.wheelsf = False
 		self.shoot = False
 		self.toss = ""
 		self.stopb = False
 		self.dribble = False
 		self.ready = False
-		self.spin = None
-		self.kin = ""
-		self.cos = ""
-		self.P1P = None
-		self.push = False
+
 
 		# handle args
 		for arg in args:
@@ -447,76 +287,7 @@ class controller:
 			else:
 				key = arg[:f]
 				val = arg[f+1:]
-			if key == "state_file":
-				self.state_file = val
-			elif key == "forever":
-				self.forever = True
-			elif key == "wheels":
-				self.wheels = 0.4
-			elif key == "stall":
-				self.wheels = 4.0
-				self.report_wheels = True
-				self.report_input = False
-			elif key == "stall-":
-				self.wheels = -4.0
-				self.report_wheels = True
-				self.report_input = False
-			elif key == "wheels-":
-				self.wheels = -0.4
-			elif key == "wheelsf":
-				self.wheelsf = True
-				self.report_wheels = True
-				self.report_input = False
-			elif key == "spin":
-				self.spin = 1.0
-			elif key == "spin-":
-				self.spin = -1.0
-			elif key == "wheels_cont":
-				self.wheels = 0.4
-				self.report_wheels = True
-				self.report_input = False
-			elif key == "spin_cont":
-				self.spin = 1.0
-				self.report_wheels = True
-				self.report_input = False
-			elif key == "push":
-				self.push = True
-			elif key == "kin":
-				self.kin = "lyp"
-				self.report_input = False
-			elif key == "lift":
-				self.kin = "l"
-				self.report_input = False
-			elif key == "yaw":
-				self.kin = "y"
-				self.report_input = False
-			elif key == "pitch":
-				self.kin = "p"
-				self.report_input = False
-			elif key == "cos":
-				self.cos = "hw"
-			elif key == "cosl":
-				self.cos = "l"
-			elif key == "cosr":
-				self.cos = "r"
-			elif key == "eyes":
-				self.cos = "y"
-			elif key == "ears":
-				self.cos = "e"
-			elif key == "wag":
-				self.cos = "w"
-			elif key == "droop":
-				self.cos = "d"
-			elif key == "wagdroop":
-				self.cos = "x"
-			elif key == "P1P":
-				self.P1P = val
-			elif key == "quiet":
-				self.report_input = False
-			elif key == "workout":
-				self.cos = "lrx"
-				self.kin = "lyp"
-			elif key == "shoot":
+			if key == "shoot":
 				self.shoot = True
 			elif key == "passl":
 				self.toss = "l"
