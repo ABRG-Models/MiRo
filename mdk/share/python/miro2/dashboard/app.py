b641fe5f74475f1aae2a9ad4ce4eef4a5c69c9e4
David B <dbxspam@gmail.com>
2019-04-15 15:50:45 +0100

    Assembling basic layout
    Using Dash Bootstrap components to assemble a basic dashboard layout

--- mdk/share/python/miro2/dashboard/app.py
+++ mdk/share/python/miro2/dashboard/app.py
@@ -1,8 +1,10 @@
 #!/home/dbuxton/mdk/share/python/miro2/dashboard/.venv/bin/python
+# -*- coding: utf-8 -*-
 # Import Dash components
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 
 import plotly.graph_objs as go
@@ -26,41 +28,599 @@ import cv2
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+arrow = {
+	'up'   : '⬆',
+	'right': '➡',
+	'down' : '⬇',
+	'left' : '⬅'
+}
 
-app.layout = html.Div(children=[
-	html.H1(children='MiRo dashboard'),
+css = {
+	'arrow': {
+		'font-size'  : 'xx-large',
+		'font-weight': 'bolder',
+		'text-align' : 'center',
+		'padding'    : '0px'
+	}
+}
 
-	html.Div(children='''
-        What's MiRo up to today?
-    '''),
+top_arrows = dbc.Row(
+	[
+		dbc.Col(
+			html.Div(
+				arrow['down'],
+				style=css['arrow'],
+				id='top-sigma'
+			),
+			width={
+				'size'  : 1,
+				'offset': 3
+			}
+		),
+		dbc.Col(
+			html.Div(
+				arrow['down'],
+				style=css['arrow'],
+				id='top-spatial'
+			),
+			width={
+				'size'  : 1,
+				'offset': 0
+			}
+		),
+		dbc.Col(
+			html.Div(
+				arrow['down'],
+				style=css['arrow']
+			),
+			width={
+				'size'  : 1,
+				'offset': 0
+			}
+		),
+		dbc.Col(
+			html.Div(
+				arrow['up'],
+				style=css['arrow']
+			),
+			width={
+				'size'  : 1,
+				'offset': 3
+			}
+		),
+		dbc.Col(
+			html.Div(
+				arrow['down'],
+				style=css['arrow'],
+				id='top-affect'
+			),
+			width={
+				'size'  : 1,
+				'offset': 1
+			}
+		)
+	],
+	no_gutters=True,
+)
 
-	# Changing component IDs will cause an error if the dashboard is still open
-	dcc.Graph(
-		id='affect-graph',
-		animate=True,
-		config={
-			'displayModeBar': False
-		},
-		style={
-			'height': '600px',
-			'width' : '600px'
-		}
-	),
+upper_group = dbc.Row(
+	[
+		dbc.Col(
+			dbc.Alert(
+				'Σ',
+				style=css['arrow'],
+				color='info'
+			),
+			width={
+				'size'  : 1,
+				'offset': 3
+			}
+		),
+		dbc.Col(
+			html.Div(
+				[
+					arrow['left'],
+					arrow['down']
+				]
+			),
+			width={
+				'size'  : 1,
+				'offset': 0
+			}
+		),
+		dbc.Col(
+			dbc.Card(
+				[
+					dbc.CardHeader('Action selection'),
+					dbc.CardBody(dbc.CardText('[Action selection graph]'))
+				]
+			),
+			width={
+				'size'  : 5,
+				'offset': 0
+			}
+		),
+		dbc.Col(
+			html.Div(
+				arrow['down'],
+				style=css['arrow'],
+				# id='top-affect'
+			),
+			width={
+				'size'  : 1,
+				'offset': 1
+			}
+		),
+	],
+	no_gutters=True
+)
 
-	dcc.Graph(
-		id='cam-graph',
-		config={
-			'displayModeBar': False
-		}
-	),
+upper_arrows = dbc.Row(
+	[
+		dbc.Col(
+			html.Div(
+				arrow['down'],
+				style=css['arrow']
+			),
+			width={
+				'size'  : 1,
+				'offset': 3
+			}
+		),
+		dbc.Col(
+			html.Div(
+				arrow['down'],
+				style=css['arrow']
+			),
+			width={
+				'size'  : 1,
+				'offset': 0
+			}
+		),
+		dbc.Col(
+			html.Div(
+				arrow['up'],
+				style=css['arrow']
+			),
+			width={
+				'size'  : 1,
+				'offset': 0
+			}
+		),
+		dbc.Col(
+			html.Div(
+				arrow['up'],
+				style=css['arrow']
+			),
+			width={
+				'size'  : 1,
+				'offset': 1
+			}
+		),
+		dbc.Col(
+			html.Div(
+				arrow['down'],
+				style=css['arrow']
+			),
+			width={
+				'size'  : 1,
+				'offset': 1
+			}
+		),
+		dbc.Col(
+			html.Div(
+				arrow['down'],
+				style=css['arrow']
+			),
+			width={
+				'size'  : 1,
+				'offset': 1
+			}
+		)
+	],
+	no_gutters=True
+)
 
-	dcc.Interval(
-		id='interval-component',
-		interval=0.25 * 1000,
-		n_intervals=0
-	)
-])
+mid_group = dbc.Row(
+	[
+		dbc.Col(
+			dbc.Card(
+				[
+					dbc.CardBody(dbc.CardText('[MiRo]'))
+				]
+			),
+			width=1
+		),
+		dbc.Col(
+			html.Div(
+				arrow['right'],
+				style=css['arrow']
+			),
+			width=1
+		),
+		dbc.Col(
+			[
+				dbc.Alert(
+					'Motor reafferent',
+					color='info'
+				),
+				dbc.Alert(
+					'Noise filter',
+					color='info'
+				)
+			],
+			width={
+				'size'  : 1,
+				'offset': 0
+			}
+		),
+		dbc.Col(
+			html.Div(
+				[
+					arrow['right'],
+					arrow['down']
+				]
+			),
+			width=1
+		),
+		dbc.Col(
+			html.Div(
+				[
+					arrow['right'],
+					'L'
+				]
+			),
+			width=1
+		),
+		dbc.Col(
+			html.Div(
+				[
+					arrow['right'],
+					arrow['up']
+				]
+			),
+			width=1
+		),
+		dbc.Col(
+			dbc.Card(
+				[
+					dbc.CardHeader('Spatial attention'),
+					dbc.CardBody(
+						[
+							dcc.Graph(
+								id='cam-graph',
+								config={
+									'displayModeBar': False
+								},
+								style={
+									'border': '1px solid'
+								}
+							)
+						]
+					)
+				]
+			),
+			width={
+				'size'  : 2,
+				'offset': 0
+			}
+		),
+		dbc.Col(
+			dbc.Card(
+				[
+					dbc.CardHeader('Affect'),
+					dbc.CardBody(
+						[
+							dcc.Graph(
+								id='affect-graph',
+								animate=True,
+								config={
+									'displayModeBar': False
+								},
+								style={
+									# 'height': '400px',
+									# 'width' : '400px',
+									'border': '1px solid'
+								}
+							)
+						]
+					)
+				]
+			),
+			width={
+				'size'  : 3,
+				'offset': 1
+			}
+		)
+	],
+	no_gutters=True
+)
+
+lower_arrows = dbc.Row(
+	[
+		dbc.Col(
+			html.Div(
+				arrow['up'],
+				style=css['arrow']
+			),
+			width={
+				'size'  : 1,
+				'offset': 2
+			}
+		),
+		dbc.Col(
+			html.Div(
+				arrow['down'],
+				style=css['arrow']
+			),
+			width={
+				'size'  : 1,
+				'offset': 0
+			}
+		),
+		dbc.Col(
+			html.Div(
+				arrow['up'],
+				style=css['arrow']
+			),
+			width={
+				'size'  : 1,
+				'offset': 1
+			}
+		),
+		dbc.Col(
+			html.Div(
+				arrow['up'],
+				style=css['arrow']
+			),
+			width={
+				'size'  : 1,
+				'offset': 1
+			}
+		),
+		dbc.Col(
+			html.Div(
+				arrow['up'],
+				style=css['arrow']
+			),
+			width={
+				'size'  : 1,
+				'offset': 1
+			}
+		),
+		dbc.Col(
+			html.Div(
+				arrow['up'],
+				style=css['arrow']
+			),
+			width={
+				'size'  : 1,
+				'offset': 0
+			}
+		),
+		dbc.Col(
+			html.Div(
+				arrow['down'],
+				style=css['arrow']
+			),
+			width={
+				'size'  : 1,
+				'offset': 0
+			}
+		),
+	],
+	no_gutters=True
+)
+
+lower_group = dbc.Row(
+	[
+		dbc.Col(
+			dbc.Card(
+				[
+					dbc.CardHeader('Body model (Motor)'),
+					dbc.CardBody(dbc.CardText('Lorem ipsum'))
+				]
+			),
+			width={
+				'size'  : 3,
+				'offset': 3
+			}
+		),
+		dbc.Col(
+			html.Div(
+				arrow['left'],
+				style=css['arrow']
+			),
+			width={
+				'size'  : 1,
+				'offset': 0
+			}
+		),
+		dbc.Col(
+			dbc.Card(
+				[
+					dbc.CardHeader('Body model (Sensory)'),
+					dbc.CardBody(dbc.CardText('Lorem ipsum'))
+				]
+			),
+			width={
+				'size'  : 1,
+				'offset': 0
+			}
+		),
+		dbc.Col(
+			dbc.Card(
+				[
+					dbc.CardHeader('Circadian rhythm'),
+					dbc.CardBody(dbc.CardText('Lorem ipsum'))
+				]
+			),
+			width={
+				'size'  : 1,
+				'offset': 1
+			}
+		),
+		dbc.Col(
+			html.Div(
+				arrow['up'],
+				style=css['arrow']
+			),
+			width={
+				'size'  : 1,
+				'offset': 0
+			}
+		),
+		dbc.Col(
+			dbc.Card(
+				[
+					dbc.CardHeader('Expression'),
+					dbc.CardBody(dbc.CardText('Lorem ipsum'))
+				]
+			),
+			width={
+				'size'  : 1,
+				'offset': 0
+			}
+		),
+	],
+	no_gutters=True
+)
+
+bottom_arrows = dbc.Row(
+	[
+		dbc.Col(
+			html.Div(
+				arrow['down'],
+				style=css['arrow']
+			),
+			width={
+				'size'  : 1,
+				'offset': 5
+			}
+		),
+		dbc.Col(
+			html.Div(
+				arrow['up'],
+				style=css['arrow']
+			),
+			width={
+				'size'  : 1,
+				'offset': 1
+			}
+		),
+		dbc.Col(
+			html.Div(
+				arrow['up'],
+				style=css['arrow']
+			),
+			width={
+				'size'  : 1,
+				'offset': 1
+			}
+		),
+		dbc.Col(
+			html.Div(
+				arrow['up'],
+				style=css['arrow']
+			),
+			width={
+				'size'  : 1,
+				'offset': 0
+			}
+		),
+		dbc.Col(
+			html.Div(
+				arrow['down'],
+				style=css['arrow']
+			),
+			width={
+				'size'  : 1,
+				'offset': 0
+			}
+		),
+	],
+	no_gutters=True
+)
+
+tooltips = html.Div(
+	[
+		dbc.Tooltip(
+			'Motor pattern output',
+			target='top-sigma'
+		),
+		dbc.Tooltip(
+			'Spatial bias, inhibition of return',
+			target='top-spatial'
+		),
+		dbc.Tooltip(
+			'Downstream effects on affective state',
+			target='top-affect'
+		),
+	]
+)
+
+
+# Dash Bootstrap CSS theme
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+# Default Dash CSS theme
+# app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div(
+	[
+		top_arrows,
+		upper_group,
+		upper_arrows,
+		mid_group,
+		lower_arrows,
+		lower_group,
+		bottom_arrows,
+		tooltips,
+		dcc.Interval(
+			id='interval-component',
+			interval=0.25 * 1000,
+			n_intervals=0
+		)
+	]
+)
+
+# This is only to suppress warnings TEMPORARILY
+app.config['suppress_callback_exceptions'] = True
+
+# app.layout = html.Div(children=[
+# 	html.H1(children='MiRo dashboard'),
+#
+# 	html.Div(children='''
+#         What's MiRo up to today?
+#     '''),
+#
+# 	# Changing component IDs will cause an error if the dashboard is still open
+# 	dcc.Graph(
+# 		id='affect-graph',
+# 		animate=True,
+# 		config={
+# 			'displayModeBar': False
+# 		},
+# 		style={
+# 			'height'         : '400px',
+# 			'width'          : '400px',
+# 			'border'         : '1px solid',
+# 		}
+# 	),
+#
+# 	dcc.Graph(
+# 		id='cam-graph',
+# 		config={
+# 			'displayModeBar': False
+# 		}
+# 	),
+#
+# 	dcc.Interval(
+# 		id='interval-component',
+# 		interval=0.25 * 1000,
+# 		n_intervals=0
+# 	)
+# ])
 
 
 
@@ -70,23 +630,29 @@ def update_affect(n):
 	affect_layout = go.Layout(
 		title={'text': 'Affect'},
 		xaxis={
-			'title'     : 'Valence',
-			'range'     : [0, 1],
-			'fixedrange': True
+			'title'         : 'Valence',
+			'range'         : [0, 1],
+			'fixedrange'    : True,
+			'showgrid'      : False,
+			'zeroline'      : False,
+			'showticklabels': False,
+			'mirror'        : True,
+			'linewidth'     : 1
 		},
 		yaxis={
-			'title'     : 'Arousal',
-			'range'     : [0, 1],
-			'fixedrange': True
+			'title'         : 'Arousal',
+			'range'         : [0, 1],
+			'fixedrange'    : True,
+			'showgrid'      : False,
+			'zeroline'      : False,
+			'showticklabels': False,
+			'mirror'        : True,
+			'linewidth'     : 1
 		}
 	)
 
 	# Update affect
-	#TODO: Try to prevent "IOError: [Errno 11] Resource temporarily unavailable" errors
-	# print('Affect queue empty? ' + str(miro_ros_data.core_affect.empty()))
-
 	affect_in = miro_ros_data.core_affect
-	# affect_in = miro_ros_data.core_affect.get()
 	if affect_in is not None:
 		affect_out = {
 			'emotion': go.Scatter(
@@ -136,14 +702,18 @@ def update_affect(n):
 		}
 
 		return {
-			'data'  : [affect_out['emotion'], affect_out['mood'], affect_out['sleep']],
+			'data'  : [
+				affect_out['emotion'],
+				affect_out['mood'],
+				affect_out['sleep']
+			],
 			'layout': affect_layout
 		}
 
 	else:
 		null_data = go.Scatter(
-			x=np.array(0),
-			y=np.array(0),
+			x=np.array(-1),
+			y=np.array(-1),
 		)
 
 		return {
@@ -244,4 +814,4 @@ if __name__ == '__main__':
 	miro_dash = {}
 
 	# Hot reloading seems to cause "IOError: [Errno 11] Resource temporarily unavailable" errors
-	app.run_server(debug=False)
+	app.run_server(debug=True)
--- mdk/share/python/miro2/dashboard/miro_interface.py
+++ mdk/share/python/miro2/dashboard/miro_interface.py
@@ -1,14 +1,14 @@
 #!/home/dbuxton/mdk/share/python/miro2/dashboard/.venv/bin/python
 
 # /usr/bin/env python
-import rospy
-
 # MiRo-E ROS interfaces
 from std_msgs.msg import Float32MultiArray, UInt32MultiArray, UInt16MultiArray, UInt8MultiArray, UInt16, UInt32, Int16MultiArray, String
 from geometry_msgs.msg import TwistStamped
 from sensor_msgs.msg import JointState, BatteryState, Imu, Range, CompressedImage
 import miro2 as miro
+import rospy
 
+# Image handling
 import cv2
 from PIL import Image
 
@@ -18,127 +18,10 @@ import numpy as np
 
 import Queue
 
-
-# class fifo:
-#
-#     def __init__(self, uncompressed):
-#         self.N = 1  # raise if we get "overflow in fifo", or to improve fps measurement
-#         self.buf = [None] * self.N
-#         self.r = 0
-#         self.w = 0
-#         self.tN = 30
-#         self.ti = 0
-#         self.td = 10
-#         self.tt = [None] * self.tN
-#         self.hz = None
-#         self.uncompressed = uncompressed
-#         self.lock = threading.Lock()
-#
-#     def push(self, frm):
-#         self.lock.acquire()
-#         try:
-#             t_frm = time.time()
-#             if self.buf[self.w] is None:
-#                 if self.uncompressed:
-#                     pb = GdkPixbuf.Pixbuf.new_from_data(frm.data,
-#                         GdkPixbuf.Colorspace.RGB, False, 8,
-#                         frm.width, frm.height, frm.step)
-#                 else:
-#                     im = cv2.imdecode(np.fromstring(frm.data, np.uint8),
-#                                 cv2.IMREAD_COLOR)
-#                     w = im.shape[1]
-#                     h = im.shape[0]
-#                     N = h * w
-#                     for i in range(0, N):
-#                         tmp = im.data[i*3+0]
-#                         im.data[i*3+0] = im.data[i*3+2]
-#                         im.data[i*3+2] = tmp
-#                     pb = im
-#                     #pb = GdkPixbuf.Pixbuf.new_from_data(im.data, GdkPixbuf.Colorspace.RGB, False, 8, w, h, w*3)
-#
-#                 self.buf[self.w] = pb
-#             else:
-#                 #print("**** frame dropped ****")
-#                 pass
-#             self.tt[self.ti] = t_frm
-#             ti_bak = self.ti - self.td
-#             if ti_bak < 0:
-#                 ti_bak = ti_bak + self.tN
-#             if not self.tt[ti_bak] is None:
-#                 dt = t_frm - self.tt[ti_bak]
-#                 self.hz = self.td / dt
-#                 if self.hz > (self.td + 3) and self.td < (self.tN - 1):
-#                     self.td = self.td + 1
-#                     #print "> ", self.td
-#                 if self.hz < (self.td - 3) and self.td > 5:
-#                     self.td = self.td - 1
-#                     #print "< ", self.td
-#             self.ti = (self.ti + 1) % self.tN
-#         finally:
-#             self.lock.release()
-#
-#     def pop(self):
-#         obj = self.buf[self.r]
-#         if not obj is None:
-#             self.buf[self.r] = None
-#             self.r = (self.r + 1) % self.N
-#         return obj
-#
-#     def latest(self):
-#         ret = None
-#         while True:
-#             obj = self.pop()
-#             if obj is None:
-#                 break
-#             ret = obj
-#         return ret
-#
-#     def freq(self):
-#         hz = self.hz
-#         if not hz is None:
-#             self.hz = None
-#         return hz
-
-
-
 # TODO: Add definitions for physical or sim. robot
+
+
 class MiroClient:
-
-	# def callback_platform_sensors(self, object):
-	# 	# # ignore until active
-	# 	# if not self.active:
-	# 	# 	return
-	#
-	# 	# store object
-	# 	self.platform_sensors = object
-	# 	print(self.platform_sensors)
-
-	@staticmethod
-	def process_frame(frame):
-		# Decode image from numpy string format
-		frame_bgr = cv2.imdecode(np.fromstring(frame.data, np.uint8), cv2.IMREAD_COLOR)
-
-		# Convert image to RGB order
-		frame_rgb = cv2.cvtColor(frame_bgr, cv2.COLOR_RGB2BGR)
-
-		# Convert to image format
-		return Image.fromarray(frame_rgb)
-
-		# FIXME: Can we convert directly from source in one go?
-		# return Image.frombytes('RGB', (320, 176), frame.data)
-
-	# TODO: Image stitching before passing images back to dashboard
-	def callback_caml(self, frame):
-		self.sensors_caml = self.process_frame(frame)
-
-	def callback_camr(self, frame):
-		self.sensors_camr = self.process_frame(frame)
-
-	def callback_core_affect(self, data):
-		self.core_affect = data
-		# self.core_affect.put(data)
-
-
 	def __init__(self):
 
 		# # set inactive
@@ -150,13 +33,14 @@ class MiroClient:
 
 		uncompressed = False
 
+		# Set topic root
 		topic_root = "/miro"
 
-		# rospy.init_node('listener', anonymous=True, disable_rostime=True)
-
+		# Subscribe to ROS topics
 		rospy.Subscriber(topic_root + "/core/affect", miro.msg.affect_state, self.callback_core_affect)
 
 		if self.opt['Uncompressed']:
+			# TODO: Uncompressed callbacks not yet tested
 			rospy.Subscriber(topic_root + "/sensors/caml", Imu, self.callback_caml)
 			rospy.Subscriber(topic_root + "/sensors/camr", Imu, self.callback_camr)
 			#
@@ -170,11 +54,6 @@ class MiroClient:
 			# rospy.Subscriber(topic_root + "/sensors/pril/compressed", CompressedImage, self.callback_pril)
 			# rospy.Subscriber(topic_root + "/sensors/prir/compressed", CompressedImage, self.callback_prir)
 			# rospy.Subscriber(topic_root + "/sensors/priw/compressed", CompressedImage, self.callback_priw)
-
-		# '''
-		# self.sub_rgbl = rospy.Subscriber(topic_root + "/core/rgbl/compressed", CompressedImage, self.callback_rgbl)
-		# self.sub_rgbr = rospy.Subscriber(topic_root + "/core/rgbr/compressed", CompressedImage, self.callback_rgbr)
-		# '''
 
 
 		# self.sub_sensors = rospy.Subscriber(topic_root + "/sensors/package", miro.msg.sensors_package,
@@ -197,6 +76,33 @@ class MiroClient:
 		self.sensors_caml = None
 		self.sensors_camr = None
 		# self.caml_fifo = fifo(self.opt['Uncompressed'])
+
+
+
+	# TODO: Image stitching before passing images back to dashboard
+	def callback_caml(self, frame):
+		self.sensors_caml = self.process_frame(frame)
+
+	def callback_camr(self, frame):
+		self.sensors_camr = self.process_frame(frame)
+
+	def callback_core_affect(self, data):
+		self.core_affect = data
+		# self.core_affect.put(data)
+
+	@staticmethod
+	def process_frame(frame):
+		# Decode image from numpy string format
+		frame_bgr = cv2.imdecode(np.fromstring(frame.data, np.uint8), cv2.IMREAD_COLOR)
+
+		# Convert image to RGB order
+		frame_rgb = cv2.cvtColor(frame_bgr, cv2.COLOR_RGB2BGR)
+
+		# Convert to image format
+		return Image.fromarray(frame_rgb)
+
+		# TODO: Can we convert directly from source in one go?
+		# return Image.frombytes('RGB', (320, 176), frame.data)
 
 
 if __name__ == "__main__":
