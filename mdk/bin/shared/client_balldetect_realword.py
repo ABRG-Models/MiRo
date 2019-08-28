f49f6a578b565c7a5c7bb60fe5cdd6e88e2e6d50
jodie528 <liws18uk@yeah.net>
2019-08-28 15:22:06 +0100

    update

--- mdk/bin/shared/client_balldetect_realword.py
+++ mdk/bin/shared/client_balldetect_realword.py
@@ -32,7 +32,7 @@ def error(msg):
 ################################################################
 
 class QLearningTable:
-    def __init__(self, action_space,learning_rate=0.2, discount=0.9, e_greedy=0.001):
+    def __init__(self, action_space,learning_rate=0.2, discount=0.9, e_greedy=0.00):
         self.lr = learning_rate
         self.gamma = discount
         self.epsilon = e_greedy
@@ -82,7 +82,7 @@ class QLearningTable:
         self.q_table.loc[state, action] += self.lr * (q_target - q_predict)  # update
 
     def import_QTable(self):
-        Q_table = pd.read_csv("/home/miro/mdk/bin/shared/qtable2.csv",index_col=0)
+        Q_table = pd.read_csv("/home/miro/mdk/bin/shared/qtable3.csv",index_col=0)
         return Q_table
 
     def save_QTable(self, Q_table):
@@ -123,14 +123,14 @@ class client_findball3:
         # loop
         while not rospy.core.is_shutdown():
             # left = self.find_ball("#DE3163", 0)
-            # if left is not None:
-            #     print "show",left[0]
-            #     output = left[1]
-            #     cv2.imshow("left", output)
-            #     cv2.waitKey()
-            #     cv2.destroyAllWindows()
-            #     print("show image")
-            #     time.sleep(1)
+            # # right = self.find_ball("#DE3163", 1)
+            # print "show",left
+            #
+            # print("show image")
+            # time.sleep(1)
+
+
+            # print "show",right
 
             # time.sleep(0.1)
             # self.kin_cos_init()
@@ -173,8 +173,8 @@ class client_findball3:
                     print "new state", state
                     time.sleep(0.01)
 
-            #     reward_list.append(tot_reward)
-            #     self.Q.epsilon = f(i, 0.1)
+                # reward_list.append(tot_reward)
+                # self.Q.epsilon = f(i, 0.1)
             # print self.Q.print_Tabel()
             # self.save_QTable(self.Q.q_table)
             # print("save success")
@@ -182,7 +182,6 @@ class client_findball3:
             # plt.plot(reward_list)
             # plt.show()
             break
-
 
     def __init__(self):
 
@@ -242,7 +241,7 @@ class client_findball3:
         self.frame_w = 0
         self.frame_h = 0
 
-    def find_ball(self, colour_str, cam_id):
+    def find_ball_r(self, colour_str, cam_id):
         # self.pause()
         if colour_str[0] != "#" and len(colour_str) != 7:
             print("colour choice should be a string in the form \"#RRGGBB\"")
@@ -295,6 +294,106 @@ class client_findball3:
         seg = cv2.erode(seg, None, iterations=2)
         seg = cv2.dilate(seg, None, iterations=2)
 
+        # cv2.imshow("seg", seg)
+        # cv2.waitKey()
+        # cv2.destroyAllWindows()
+
+        # get circles
+        circles = cv2.HoughCircles(seg, cv2.HOUGH_GRADIENT, 1, 40, param1=10, param2=20, minRadius=0, maxRadius=0)
+
+        # Get largest circle
+        max_circle = None
+        max_circle_norm = [None, None, None]
+        if circles is not None:
+            self.max_rad = 0
+            circles = np.uint16(np.around(circles))
+
+            for c in circles[0, :]:
+                cv2.circle(seg, (c[0], c[1]), c[2], (0, 255, 0), 2)
+
+                if c[2] > self.max_rad:
+                    self.max_rad = c[2]
+                    max_circle = c
+                    max_circle_norm[0] = int(round(((max_circle[0] - im_centre_w) / im_centre_w) * 100.0))
+                    max_circle_norm[1] = int(round(-((max_circle[1] - im_centre_h) / im_centre_h) * 100.0))
+                    max_circle_norm[2] = int(round((max_circle[2] / im_centre_w) * 100.0))
+                # Debug Only
+                cv2.circle(output, (max_circle[0], max_circle[1]), max_circle[2], (0, 255, 0), 2)
+                # cv2.circle(output, (max_circle[0], max_circle[1]), 1, (0, 255, 0), 2)
+                location_str = "x: " + str(max_circle_norm[0]) + "," + "y: " + str(
+                    max_circle_norm[1]) + "," + "r: " + str(max_circle[2])
+                text_y_offset = 18
+                for i, line in enumerate(location_str.split(",")):
+                    text_y = max_circle[1] - text_y_offset + i * text_y_offset
+                    cv2.putText(output, line, (max_circle[0] + 5, text_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 3)
+                    cv2.putText(output, line, (max_circle[0] + 5, text_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0),
+                                1)
+
+        else:
+            return None
+
+        # cv2.imshow("output", output)
+        # cv2.waitKey()
+        # cv2.destroyAllWindows()
+        return max_circle_norm
+
+    def find_ball_l(self, colour_str, cam_id):
+        # self.pause()
+        if colour_str[0] != "#" and len(colour_str) != 7:
+            print("colour choice should be a string in the form \"#RRGGBB\"")
+            return
+        if cam_id < 0 or cam_id > 1:
+            return
+        # if prop < 0 or prop > 2:
+        #     return
+
+        # create colour code from user selected colour
+        red = int(colour_str[1:3], 16)
+        green = int(colour_str[3:5], 16)
+        blue = int(colour_str[5:7], 16)
+        bgr_colour = np.uint8([[[blue, green, red]]])
+        hsv_colour = cv2.cvtColor(bgr_colour, cv2.COLOR_BGR2HSV)
+
+        # extract boundaries for masking image
+        target_hue = hsv_colour[0, 0][0]
+        lower_bound = np.array([0, 90, 70])
+        upper_bound = np.array([10, 255, 255])
+
+        if np.shape(self.cam_left_image) != () and np.shape(self.cam_right_image) != ():
+            # convert camera image to HSV colour space
+            if cam_id == miro.constants.CAM_L:
+                hsv_image = cv2.cvtColor(self.cam_left_image, cv2.COLOR_BGR2HSV)
+                output = self.cam_left_image.copy()
+            else:
+                hsv_image = cv2.cvtColor(self.cam_right_image, cv2.COLOR_BGR2HSV)
+                output = self.cam_right_image.copy()
+        else:
+            return None
+
+        # cv2.imshow("hsv", output)
+        # cv2.waitKey()
+        # cv2.destroyAllWindows()
+
+        im_h = np.size(hsv_image, 0)
+        im_w = np.size(hsv_image, 1)
+        im_centre_h = im_h / 2.0
+        im_centre_w = im_w / 2.0
+        cv2.line(output, (0, int(round(im_centre_h))), (im_w, int(round(im_centre_h))), (100, 100, 100), 1)
+        cv2.line(output, (int(round(im_centre_w)), 0), (int(round(im_centre_w)), im_h), (100, 100, 100), 1)
+
+        # mask image
+        mask = cv2.inRange(hsv_image, lower_bound, upper_bound)
+        seg = mask
+
+        # Do some processing
+        seg = cv2.GaussianBlur(seg, (11, 11), 0)
+        seg = cv2.erode(seg, None, iterations=2)
+        seg = cv2.dilate(seg, None, iterations=2)
+
+        # cv2.imshow("seg", seg)
+        # cv2.waitKey()
+        # cv2.destroyAllWindows()
+
         # get circles
         circles = cv2.HoughCircles(seg, cv2.HOUGH_GRADIENT, 1, 40, param1=10, param2=20, minRadius=0, maxRadius=100)
 
@@ -314,7 +413,6 @@ class client_findball3:
                     max_circle_norm[0] = int(round(((max_circle[0] - im_centre_w) / im_centre_w) * 100.0))
                     max_circle_norm[1] = int(round(-((max_circle[1] - im_centre_h) / im_centre_h) * 100.0))
                     max_circle_norm[2] = int(round((max_circle[2] / im_centre_w) * 100.0))
-                    print("rrrrrrrrrr",target_hue)
                 # Debug Only
                 cv2.circle(output, (max_circle[0], max_circle[1]), max_circle[2], (0, 255, 0), 2)
                 # cv2.circle(output, (max_circle[0], max_circle[1]), 1, (0, 255, 0), 2)
@@ -330,7 +428,7 @@ class client_findball3:
         else:
             return None
 
-        # cv2.imshow("hsv", output)
+        # cv2.imshow("output", output)
         # cv2.waitKey()
         # cv2.destroyAllWindows()
         return max_circle_norm
@@ -345,20 +443,20 @@ class client_findball3:
     def get_state(self):
         time.sleep(0.05)
         state = []
-        leftCamera = self.find_ball("#DE3163", 0)  # C71585 DE3163
-        rightCamera = self.find_ball("#DE3163", 1)
+        leftCamera = self.find_ball_l("#DE3163", 0)  # C71585 DE3163
+        rightCamera = self.find_ball_r("#DE3163", 1)
 
         print "left", leftCamera
         print "right", rightCamera
 
         if not leftCamera is None:
             if leftCamera[0] < 0:
-                if leftCamera[2] < 13:
+                if leftCamera[2] < 20:
                     state.append(1)
                 else:
                     state.append(2)
             else:
-                if leftCamera[2] < 13:
+                if leftCamera[2] < 20:
                     state.append(3)
                 else:
                     state.append(4)
@@ -367,12 +465,12 @@ class client_findball3:
 
         if not rightCamera is None:
             if rightCamera[0] < 0:
-                if rightCamera[2] < 13:
+                if rightCamera[2] < 50:
                     state.append(1)
                 else:
                     state.append(2)
             else:
-                if rightCamera[2] < 13:
+                if rightCamera[2] < 50:
                     state.append(3)
                 else:
                     state.append(4)
@@ -448,7 +546,7 @@ class client_findball3:
     def action_stepback(self):
         start = datetime.datetime.now()
 
-        while (self.sonar < 0.05):
+        while (True):
             end = datetime.datetime.now()
             if (end - start).seconds > 1:
                 self.velocity.twist.linear.x = 0.0
